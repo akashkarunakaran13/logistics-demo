64092df8f75705133ae3e00ef37963d7efd0e67f
akashkarunakaran13 <yourGitHubemail>
2026-05-12 13:53:54 +0100

    Replace Geist fonts with Inter and Roboto Mono

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    turbo: {}, // ✅ must be an object, not a boolean
-  },
-  images: {
-    domains: ["images.unsplash.com", "vercel.com"],
+    turbo: {}, // must be an object, not a boolean
   },
   eslint: {
     ignoreDuringBuilds: true,
